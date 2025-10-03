8dd7949d5b7aedcafe2a6b527e22da31e10a25c2
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-10-03 10:45:08 -0300

    feat(auth): adicionar rota de login como tela inicial

---
+++ app/auth/login.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 20 }}>Tela de Login</Text>
+    </View>
+  );
+}
