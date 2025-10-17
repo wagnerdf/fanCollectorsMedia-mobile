84fa18c086be458a3f7926103e8e77b53dc57fb0
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-10-17 11:05:20 -0300

    fix: bloqueio da rotação de tela para modo retrato (vertical) em toda a aplicação

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import * as ScreenOrientation from "expo-screen-orientation";
 import AuthStack from "./app/auth/AuthStack";
 
 export default function App() {
+   useEffect(() => {
+    // 🔒 Trava a orientação em modo retrato
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+  }, []);
   return (
     <NavigationContainer>
       <AuthStack />
