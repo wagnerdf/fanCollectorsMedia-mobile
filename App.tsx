663d8a884e02877a41ecb812f3e8954dd6df0d78
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-11-28 11:29:15 -0300

    fix: ajustes cores das barras superior e inferior e corrigir import removido do app.json

--- App.tsx
+++ App.tsx
@@ -2,12 +2,19 @@ import React, { useEffect } from "react";
 import { Slot } from "expo-router";
 import * as ScreenOrientation from "expo-screen-orientation";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { StatusBar } from "react-native";
+import { StatusBar, Platform } from "react-native";
+
+import changeNavigationBarColor from "react-native-navigation-bar-color";
 
 export default function App() {
   useEffect(() => {
     // 🔒 Trava a orientação em modo retrato (apenas vertical)
     ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+
+      // 🎨 Configura a barra inferior (Android apenas)
+    if (Platform.OS === "android") {
+      changeNavigationBarColor("#ffffff", true); // cor da barra + ícones escuros
+    }
   }, []);
 
 return (
@@ -15,7 +22,7 @@ return (
       <StatusBar
         barStyle="dark-content" // texto da barra em escuro
         translucent={false}     // evita sobreposição
-        backgroundColor="#fff"  // cor da barra de status
+        backgroundColor="#00BFA6"  // cor da barra de status
       />
       <Slot />
     </SafeAreaProvider>
